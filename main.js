a9e4cfaec0c3d2ed9d25d3b2747bbcf596e1bb36
Morgane Chang <morganechang@gmail.com>
2017-02-15 11:14:02 -0500

    + return word, correct letters show

--- main.js
+++ main.js
@@ -2,36 +2,63 @@
 $(document).ready(function(){
 	console.log("ready!");
 	
-	var words = ["feminism", "unity"];
-
-	function startGame(){
+	var words = ["feminism", "unity", "strength", "power", "emma!", "amen"];
+	
+	$("#startGame").on("click", function(){
+	console.log("the player wants to start the game!");
+	$("#start-game").hide();  // or animate and move to corner.
+	$("#hidden-game").show();
+	$("body").css("background-image", "url(https://pbs.twimg.com/profile_images/447374371917922304/P4BzupWu.jpeg)");
+	});
+		
 		//happens on user click button
 		//hide button
 		//reveal input box + blankFields
 		//choose random word
-	}
+	
 
 	function chooseRandomWordArray()  {   // objects v. methods() v. functions weNamedFunction  // method() is passed ONTO an object. ie: .log needs to be attached to an object (log)
-	// RETURN a random word from our 'words' array (as an array: F, E, M)
-	// math.Random math.Floor 
-	console.log(words[0].split(""));
-	return words[0].split("");  // we could do "e" every "e" encounters
+		var randomWord = function(){
+			var index = Math.floor(Math.random() * words.length);
+			return words[index];	
+		}
+	
+	var chosenWord = randomWord(this);
+	console.log(chosenWord);
+	console.log(chosenWord.split(""));
+	var chosenWordSplit = chosenWord.split("");
+	// $(".correct-letters").append(chosenWord); //.html() also works
+	$(".correct-letters").append(chosenWordSplit).replaceWith("_");
+	// $(".correct-letters").append(chosenWordSplit).replace(/\./g , "_");
+
+	return chosenWord.split("");
+	
+	// console.log(words[0].split(""));
+	// return words[0].split("");  // we could do "e" every "e" encounters
 	// a return statement always kills a function. it must be the LAST thing. "return" can kill any other code from executing.
 	}
-
+	
 	var wordArray = chooseRandomWordArray();
 
+	//show blank spaces to represent each letter
+	
 	function guessLetter() {
-		var guess = "e"; // this has to be dynamic, something you can call everytime
-
+		var guess = $("#guess-input").val();
+		// var guess = "e"; // this has to be dynamic, something you can call everytime
 		for (var i=0; i < wordArray.length; i++) {  // length = 8, but our i only goes up to 7
-			debugger
 		if (guess == wordArray[i]) {
 			console.log("Yes, there is an " + guess + ".");
+			$(".correct-letters").append(guess);
 		} else{
-			console.log("nope!")
+			console.log("There is not that letter!")
+			$("#guess-input").val("");
 		};
-		}
+	}
+
+
+
+ 			// call word array.length, then allocate space for each.
+
 
 			// where it starts, ends, increments 
 			// i = index
@@ -41,11 +68,15 @@ $(document).ready(function(){
 	}
 
 
-// 
+	// 
+	$(".guess-button").on("click", function(){
+		guessLetter();	
+	});
 
 	
+	
 
-guessLetter()
+// guessLetter()
 
 
 
