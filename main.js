6f36e8ef20e3af75a875140bc3942e7c699f5861
Morgane Chang <morganechang@gmail.com>
2017-02-21 19:34:28 -0500

    + replace word with "-" and "," with " " --> if yes replace with correct letter.
    declare open arrays var statements at beginning (available to anyone)
    var wordArray = [] ; wordArray —> wordHint even though not technically
    declared

--- main.js
+++ main.js
@@ -3,87 +3,103 @@ $(document).ready(function(){
 	console.log("ready!");
 	
 	var words = ["feminism", "unity", "strength", "power", "emma!", "amen"];
-	
-	$("#startGame").on("click", function(){
-	console.log("the player wants to start the game!");
-	$("#start-game").hide();  // or animate and move to corner.
-	$("#hidden-game").show();
-	$("body").css("background-image", "url(https://pbs.twimg.com/profile_images/447374371917922304/P4BzupWu.jpeg)");
-	});
-		
+
+	var wordArray = [];   //var wordArray = []; // we're declaring this is a variable available to EVERYONE (and we assign value to it various times)
+	var wordHint = [];
+	var badGuesses = 0 //increase bad guesses by 1 i++
+
+
+	$("#start-game").on("click", function() {
+		console.log("the player wants to start the game!");
+		$("#start-game").hide();  // or animate and move to corner.
+		$("h1").hide();
+		$("h2").show();
+		$("#hidden-game").show();
+		$("#directions").show();
+		$(".chosen-word").show().css("color", "red");
+		$("body").css("background-image", "url(https://s-media-cache-ak0.pinimg.com/originals/da/3d/fc/da3dfcf00e3ce5a9fca0a177703902af.jpg)");
+		wordArray = chooseRandomWordArray();
+		displayWordHint();
+	});	
+
 		//happens on user click button
 		//hide button
 		//reveal input box + blankFields
 		//choose random word
-	
 
 	function chooseRandomWordArray()  {   // objects v. methods() v. functions weNamedFunction  // method() is passed ONTO an object. ie: .log needs to be attached to an object (log)
-		var randomWord = function(){
-			var index = Math.floor(Math.random() * words.length);
-			return words[index];	
+		// var randomWord = function(){
+		var index = Math.floor(Math.random() * words.length);
+		// return words[index];
+		var chosenWord = words[index]
+		// debugger;
+		console.log(chosenWord);
+		console.log(chosenWord.split(""));
+		return chosenWord.split("");
+	}	
+
+	function displayWordHint() {
+		for (var i=0; i< wordArray.length; i++) {
+			wordHint.push("_");
 		}
-	
-	var chosenWord = randomWord(this);
-	console.log(chosenWord);
-	console.log(chosenWord.split(""));
-	var chosenWordSplit = chosenWord.split("");
-	// $(".correct-letters").append(chosenWord); //.html() also works
-	$(".correct-letters").append(chosenWordSplit).replaceWith("_");
-	// $(".correct-letters").append(chosenWordSplit).replace(/\./g , "_");
-
-	return chosenWord.split("");
-	
-	// console.log(words[0].split(""));
-	// return words[0].split("");  // we could do "e" every "e" encounters
-	// a return statement always kills a function. it must be the LAST thing. "return" can kill any other code from executing.
+		fields = wordHint.toString().replace(/,/g," ");  //replace(/aA/g,"_") // turning array into a string .toString()
+		$(".chosen-word").text(fields);
 	}
-	
-	var wordArray = chooseRandomWordArray();
 
+	function updateWordHint(letter) {  // this is a parameter and it will blow up if not called; shows that we are expecting something.
+		for (var i=0; i < wordArray.length; i++) {  // length = 8, but our i only goes up to 7
+			if (letter == wordArray[i]) {
+				wordHint[i] = letter;
+				fields = wordHint.toString().replace(/,/g," ");  //replace(/aA/g,"_")
+				$(".chosen-word").text(fields);	
+			} 
+		};
+	}
+		// console.log(words[0].split(""));
+		// return words[0].split("");  // we could do "e" every "e" encounters
+		// a return statement always kills a function. it must be the LAST thing. "return" can kill any other code from executing.
+		
 	//show blank spaces to represent each letter
 	
 	function guessLetter() {
 		var guess = $("#guess-input").val();
 		// var guess = "e"; // this has to be dynamic, something you can call everytime
 		for (var i=0; i < wordArray.length; i++) {  // length = 8, but our i only goes up to 7
-		if (guess == wordArray[i]) {
-			console.log("Yes, there is an " + guess + ".");
-			$(".correct-letters").append(guess);
-		} else{
-			console.log("There is not that letter!")
-			$("#guess-input").val("");
+			if (guess == wordArray[i]) {
+				updateWordHint(guess);  // here, (guess) is an argument
+				console.log("Yes, there is an " + guess + ".");
+			} else {
+				console.log("There is not that letter!")
+				$(".bad-guesses").append(guess);
+				$(".bad-guesses").show();
+				$("#guess-input").val("");
+			}
 		};
-	}
-
-
+	};
 
  			// call word array.length, then allocate space for each.
-
-
 			// where it starts, ends, increments 
-			// i = index
 
 		//get value from input field
 		//loop: compare that letter to other letters in our array (for)
-	}
-
+	
 
 	// 
 	$(".guess-button").on("click", function(){
 		guessLetter();	
+		// displayWordHint();
 	});
-
-	
-	
 
 // guessLetter()
 
+});
 
 
-})
 
-
-//html:  start + input field + 
+//1. if you lose... keep track of every wrong guess - we can only have 7 before you DIE
+//2. if you win.....
+//3. add modal with gif (under intro tags)
+//4. add bad letters
 
 
 // user typers
